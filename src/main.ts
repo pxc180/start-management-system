319e9f0ec445d63a9afe2bc1c781edb9f27521cf
pengxincheng <pengxiaocheng180@163.com>
2022-06-28 10:28:17 +0800

    chore: arco design 组件库按需引入

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import ArcoVue from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.less';
 
-createApp(App).mount('#app');
+createApp(App).use(ArcoVue, { componentPrefix: 'arco' }).mount('#app');
