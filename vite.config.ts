ade63bc4296e933362cf57a9fe2c3b14a8ca6237
pengxincheng <pengxiaocheng180@163.com>
2022-06-29 17:21:08 +0800

    chore: 配置别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { createStyleImportPlugin } from 'vite-plugin-style-import';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   plugins: [
     vue(),
     createStyleImportPlugin({
@@ -11,14 +17,14 @@ export default defineConfig({
         {
           libraryName: '@arco-design/web-vue',
           esModule: true,
-          resolveStyle: name => {
+          resolveStyle: (name) => {
             // css
             // return `@arco-design/web-vue/es/${name}/style/css.js`;
             // less
             return `@arco-design/web-vue/es/${name}/style/index.js`;
-          }
-        }
-      ]
-    })
-  ]
+          },
+        },
+      ],
+    }),
+  ],
 });
