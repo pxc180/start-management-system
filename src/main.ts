132b10879d3b94eee61680d8cb266e0116627b6c
pengxincheng <pengxiaocheng180@163.com>
2022-07-04 17:17:54 +0800

    feat: 页面基本布局

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import ArcoVue from '@arco-design/web-vue';
+import '@/assets/style/global.less';
 import '@arco-design/web-vue/dist/arco.less';
 
-createApp(App)
-  .use(router)
-  .use(ArcoVue, { componentPrefix: 'arco' })
-  .mount('#app');
+createApp(App).use(router).use(ArcoVue).mount('#app');
